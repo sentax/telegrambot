cd2bce4197efa909d6757e06950b673af5e7cf89
teatop <teatop>
2016-10-31 11:22:53 -0700

    make it better

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include('TLG.php');
 $update = json_decode(file_get_contents('php://input'));
 $tg=new Telegram('283971445:AAGrn2U-_27P-6ABvVVAVXI_zsnMJ1PSadA');
 
-$tg->sendMessage($update->message->chat->id,$update->message->chat->text);
+$tg->sendMessage($update->message->chat->id,$update->message->text);
 
 
 
