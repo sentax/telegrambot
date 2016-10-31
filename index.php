c3683366f54a628138a36a350261758f9da4d5d8
teatop <teatop>
2016-10-31 11:01:03 -0700

    make it better

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?
-include('TLG.php');
+//include('TLG.php');
 
-$tg=new Telegram('283971445:AAGrn2U-_27P-6ABvVVAVXI_zsnMJ1PSadA');
+//$tg=new Telegram('283971445:AAGrn2U-_27P-6ABvVVAVXI_zsnMJ1PSadA');
 
-$tg->sendMessage('hiiii');
+//$tg->sendMessage('hiiii');
+
+$token='283971445:AAGrn2U-_27P-6ABvVVAVXI_zsnMJ1PSadA';
+$user_id = '@imsentax';
+        $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?chat_id='.$user_id;
+        $url .= '&text=' .$message."&parse_mode=HTML";
+    
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec ($ch);
+        curl_close ($ch);
+
+
+
 
 echo("okk");
 
