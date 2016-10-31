cb8b671c06d9af274c8d75ab32c922696ff853d1
teatop <teatop>
2016-10-31 13:22:44 -0700

    make it better

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ $tg->sendPhoto("arkshoe.com","http://rayanbartar.com/uploads/portfolio/small/ark
 }else if($msg=="خدمات"){
 	$tg->sendMessage($chatid,"منوی خدمات","&reply_markup=".json_encode($kyb1));
 }else if($msg=="طراحی سایت"){
-	$tg->sendMessage($chatid,"[طراحی سایت](http://www.rayanbartar.com/)","&reply_markup=".json_encode($kyb1));
+	$tg->sendMessage($chatid,'<a href="http://rayanbartar.com/">طراحی سایت</a>',"&reply_markup=".json_encode($kyb1));
 }
 else
 $tg->sendMessage($chatid,"این فقط یک ربات جهت تست میباشد و امکانات دیگری ارایه نمی شود.","&reply_markup=".json_encode($kyb));
