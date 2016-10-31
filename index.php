e8956200a8264c718c65bdc8159a79b8fb051204
teatop <teatop>
2016-10-31 11:14:01 -0700

    make it better

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?
 include('TLG.php');
-
+$update = json_decode(file_get_contents('php://input'));
 $tg=new Telegram('283971445:AAGrn2U-_27P-6ABvVVAVXI_zsnMJ1PSadA');
 
-$tg->sendMessage('hiiii');
+$tg->sendMessage($update);
 
 
-$update = json_decode(file_get_contents('php://input'));
 
-echo($update);
+
+
 
 
 ?>
